f5e8b5d833c41f97c258feb7f7a76df62682f203
Shriram Jagannathan <sjagannathan@nvidia.com>
2024-03-07 17:44:39 -0800

    Added tests to test meshing routines.

--- tests/unit/test_mesh.py
+++ tests/unit/test_mesh.py
@@ -2,7 +2,17 @@ import cunumeric as np
 import numpy as num
 import pytest
 
-from pyminiweather.mesh import meshgrid
+from pyminiweather.mesh import meshgrid, MeshData
+
+
+@pytest.fixture
+def params():
+    parameters = {}
+    parameters = {"nx": 64, "nz": 32, "xlen": 2e4, "zlen": 1e4, "hs": 2}
+    parameters["dx"] = parameters["xlen"] / parameters["nx"]
+    parameters["dz"] = parameters["zlen"] / parameters["nz"]
+
+    return parameters
 
 
 def test_meshgrid():
@@ -25,8 +35,71 @@ def test_meshgrid():
     assert num.allclose(z_np, z_num)
 
 
-# TODO
-# Test MeshData
+def test_mesh_int_ext(params):
+    m = MeshData(params)
+    xx, zz = m.get_mesh_int_ext()
+
+    x = np.linspace(
+        -params["hs"] * params["dx"],
+        (params["nx"] + params["hs"]) * params["dx"],
+        params["nx"] + 2 * params["hs"],
+        endpoint=False,
+    )
+
+    z = np.linspace(
+        -params["hs"] * params["dz"],
+        (params["nz"] + params["hs"]) * params["dz"],
+        params["nz"] + 2 * params["hs"],
+        endpoint=False,
+    )
+
+    assert np.allclose(x, xx[0, :])
+    assert np.allclose(z, zz[:, 0])
+
+
+def test_mesh_cell_centers(params):
+    m = MeshData(params)
+    xx, zz = m.get_mesh_cell_centers()
+
+    x = np.linspace(
+        params["dx"] / 2.0,
+        params["xlen"] + params["dx"] / 2.0,
+        params["nx"],
+        endpoint=False,
+    )
+    z = np.linspace(
+        params["dz"] / 2.0,
+        params["zlen"] + params["dz"] / 2.0,
+        params["nz"],
+        endpoint=False,
+    )
+
+    assert np.allclose(x, xx[0, :])
+    assert np.allclose(z, zz[:, 0])
+
+
+def test_mesh_vertical_cell_edges(params):
+    m = MeshData(params)
+    zz = m.get_mesh_vertical_cell_edges()
+    z = np.linspace(
+        0.0, (params["nz"] + 1) * params["dz"], params["nz"] + 1, endpoint=False
+    )
+
+    assert np.allclose(z, zz)
+
+
+def test_mesh_vertical_cell_centers_int_ext(params):
+    m = MeshData(params)
+    zz = m.get_mesh_vertical_cell_centers_int_ext()
+
+    z = np.linspace(
+        (-params["hs"] + 0.5) * params["dz"],
+        (params["nz"] + params["hs"] + 0.5) * params["dz"],
+        params["nz"] + 2 * params["hs"],
+        endpoint=False,
+    )
+
+    assert np.allclose(z, zz)
 
 
 if __name__ == "__main__":
